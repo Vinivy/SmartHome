80a6938efe24611a6908bdc8d345f5f0e23e05fb
Vinivy Gabriel <vinivygabriel8@gmail.com>
2025-02-11 22:57:03 -0300

    Funções Finger tudo ok

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, Text, View } from 'react-native';
+import { styles } from './Styles'; 
+import * as LocalAuthentication from 'expo-local-authentication';
+import { useEffect, useState } from 'react';
+
+import ComodsIcon from './assets/Modelos3D/ComodosIcon.png';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //função assincona da API
+ async function verifyAuthehtication() {
+  //verificar se o dispositivo tem o suporte para o API
+  const compitable = await LocalAuthentication.hasHardwareAsync();
+  console.log(compitable);
+
+  //verificar se o disponsitivo funciona outros recursos da API. com mapeação dos tipos que ira funcionar
+  const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+  console.log(types.map( (type) => LocalAuthentication.AuthenticationType[type]));
+ }
+
+ //função para verificar se o usuario tem cadastro de biometria
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    console.log(isBiometricEnrolled);
+
+    // se não tiver suporte para biometria
+    if (!isBiometricEnrolled){
+      return Alert.alert('Biometria não cadastrada', 'cadastre no dispositivo!');
+    }
+
+    // se tiver suporte para biometria 
+    const auth = await LocalAuthentication.authenticateAsync({
+      //mensagem que ira aparecer na tela
+      promptMessage: 'Autentique-se para acessar',
+      //mensagem que ira aparecer no botão de cancelar
+      fallbackLabel: 'Biometria não conhecida',
+    });
+
+    //verificar se a autenticação foi bem sucedida, ira retornar um booleano
+    setIsAuthenticated(auth.success);
+  }
+
+ useEffect(() => {
+    verifyAuthehtication();
+ }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.PrimaryScreen}>
+      <View style={styles.IconCircule}>
+      <Image 
+      style={styles.IconComodos} 
+      source={ComodsIcon} />
+        Usuario tem acesso? : {isAuthenticated ? 'Sim' : 'Não'}
+      </View>
+
+      <View>
+        <Text>Easily Control
+        Your Home</Text>
+      </View>
+      <Button 
+      title="Verificar Autenticação" 
+      onPress={handleAuthentication} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ Styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    PrimaryScreen: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      paddingTop: 100,
+    },
+
+    IconCircule:{
+      width: 259,
+      height: 272,
+      borderRadius: '50%',
+      backgroundColor: '#001524',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,    
+    },
+
+    IconComodos:{
+        width: 345.28,
+        height: 337
+    },
+});
